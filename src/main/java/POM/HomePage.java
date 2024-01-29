844695311cfdbf68dc8828e8e8d1891807a745bc
Natalia Zayukova <Natalia.zayukova@ringcentral.com>
2024-01-29 10:35:02 +0100

    test

--- src/main/java/POM/HomePage.java
+++ src/main/java/POM/HomePage.java
@@ -3,9 +3,7 @@ import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.How;
 
 import java.util.ArrayList;
